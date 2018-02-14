cf2891936c8b12938074ff5b7a6c9abe8f26f4a5
mikerhyssm <mike.smith@zestia.com>
2018-02-14 14:02:57 +0000

    first commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+## Starter for node typescript
+
+### Install
+
+```bash
+yarn
+```
+
+### Running tests
+
+```bash
+yarn test
+```
+
+### Linting
+
+```bash
+yarn lint
+```
---
+++ src/App.spec.ts
@@ -0,0 +1,11 @@
+const assert = require('assert');
+import  App  from "./App";
+
+
+describe('test', () => {
+
+  it('should run tests', () => {
+    assert.equal(true, true);
+  });
+
+});
