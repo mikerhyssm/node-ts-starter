b6cf9322ebaca55c3f098b1d038742ce8ffb02f5
mikerhyssm <mike.smith@zestia.com>
2018-03-07 13:20:23 +0000

    Ascii tree solution

--- src/App.spec.ts
+++ src/App.spec.ts
@@ -2,10 +2,56 @@ const assert = require('assert');
 import  App  from "./App";
 
 
-describe('test', () => {
+describe('getTreeLayer', () => {
 
-  it('should run tests', () => {
-    assert.equal(true, true);
+  it('should return a string of correct number of given characters ', () => {
+    const layer = App.getTreeLayer(3, '#', 3);
+    assert.equal(layer, '###');
+  });
+
+  it('should include indentation if less than basewidth', () => {
+    const layer = App.getTreeLayer(3, '#', 5);
+    assert.equal(layer, ' ###');
+  });
+
+});
+
+describe('getLayers', () => {
+
+  it('should return an array of layer sizes', () => {
+    const layer = App.getLayers(3);
+    assert.deepEqual(layer, [1, 3]);
+  });
+
+  it('should return an array of layer sizes', () => {
+    const layer = App.getLayers(7);
+    assert.deepEqual(layer, [1, 3, 5, 7]);
+  });
+
+});
+
+describe('getTree', () => {
+
+  it('should return tree', () => {
+    const layer = App.getTree(3, '€', '£');
+    assert.deepEqual(layer, [' £', '£££', '€€€' ]);
+  });
+
+});
+
+describe('getIndentation', () => {
+
+  it('should add indentation', () => {
+    const indentation = App.getIndentation(5, 3);
+    assert.equal(indentation, 1);
+  });
+
+});
+
+describe('testCases', () => {
+
+  it('should do tests', () => {
+    App.testCases();
   });
 
 });
--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,54 @@
+import { Observable } from 'rxjs/Observable';
+import { range } from 'rxjs/observable/range';
+
 class App {
 
-  constructor () {
+  testCases() {
+    const treeOne = this.getTree(3, '#', '*');
+    const treeTwo = this.getTree(13, '=', '+');
 
+    treeOne.map(line => console.log(line));
+    console.log('\n');
+    treeTwo.map(line => console.log(line));
+  }
+
+  getTree(baseWidth, baseCharacter, leafCharacter) {
+    let layers = this.getLayers(baseWidth)
+      .map(layer => this.getTreeLayer(layer, leafCharacter, baseWidth));
+
+    layers.push(this.getTreeLayer(3, baseCharacter, baseWidth));
+    return layers
+  }
+
+  getLayers(baseWidth) {
+    let layers = [];
+    const layersSource = range(1, baseWidth);
+
+    layersSource.subscribe((value) => {
+      if (value % 2 !== 0) {
+        layers.push(value);
+      }
+    });
+
+    return layers;
+  }
+
+
+  getTreeLayer(length, character, baseWidth): string {
+    let layer = "";
+    const spaceCharacter = " ";
+    const indentation = this.getIndentation(baseWidth, length);
+    const indentSource = range(1, indentation);
+    const lenghtSource = range(1, length);
+
+    indentSource.subscribe(() => layer = layer.concat(spaceCharacter));
+    lenghtSource.subscribe(() => layer = layer.concat(character));
+
+    return layer;
+  }
+
+  getIndentation(baseWidth, layerWidth) {
+    return (baseWidth - layerWidth)/2;
   }
 
 }
